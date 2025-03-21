ff4955649149f1c730b51e5626314e52c8ef31c1
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-03-21 17:20:23 +0200

    feat: Added file parser

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import { Command } from 'commander';
+import fileParser from './fileParser.js';
 
 const program = new Command();
 
@@ -8,6 +9,12 @@ program
   .version('0.0.1')
   .argument('<filepath1>')
   .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    const file1 = fileParser(filepath1);
+    const file2 = fileParser(filepath2);
+    console.log(JSON.stringify(file1, null, 2));
+    console.log(JSON.stringify(file2, null, 2));
+  })
   .helpOption('-V, --version', 'output the version number')
   .option('-f, --format [type]', 'output format')
   .helpOption('-h, --help', 'display help for command');
---
+++ src/fileParser.js
@@ -0,0 +1,22 @@
+import fs, { read } from 'fs';
+import path from 'path';
+import yaml from 'js-yaml';
+
+const fileParser = (file) => {
+  try {
+    const absolutePath = path.resolve(process.cwd(), file);
+    const extOfFile = path.extname(absolutePath);
+    const readFile = fs.readFileSync(absolutePath, 'utf-8');
+    if (extOfFile === '.json') {
+      return JSON.parse(readFile);
+    } else if (extOfFile === '.yml' || extOfFile === '.yaml') {
+      return yaml.load(readFile);
+    } else {
+      throw new Error(`Unsupported file format: ${extOfFile}`);
+    }
+  } catch (error) {
+    throw new Error(`Error reading file: ${error.message}`);
+  }
+};
+
+export default fileParser;
