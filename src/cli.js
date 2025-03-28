391bd74225768b4ff3451b07bef49275c9304288
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-03-28 20:31:50 +0200

    feat: stylish is default formatter

--- src/cli.js
+++ src/cli.js
@@ -9,16 +9,18 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .action((filepath1, filepath2) => {
+  .action((filepath1, filepath2, options) => {
     const file1 = fileParser(filepath1);
     const file2 = fileParser(filepath2);
-    const result = compareObjects(file1, file2);
-    console.log(stylish(result));
+    const diffTree = compareObjects(file1, file2);
+    if (options.format === 'stylish') {
+      console.log(stylish(diffTree));
+    }
   })
   .helpOption('-V, --version', 'output the version number')
-  .option('-f, --format [type]', 'output format')
   .helpOption('-h, --help', 'display help for command');
 
 program.parse(process.argv);
