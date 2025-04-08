a2421bce87dd297b0034cc25560ce4136495df95
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-04-08 15:58:11 +0300

    fix: fixed case when file paths has not been provided

--- src/cli.js
+++ src/cli.js
@@ -8,15 +8,24 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1')
+  .version('1.0.0')
   .option('-f, --format [type]', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2, options) => {
-    const file1 = fileParser(filepath1);
-    const file2 = fileParser(filepath2);
-    const diffTree = compareObjects(file1, file2);
-    console.log(formatCase(diffTree, options.format));
+    if (!filepath1 || !filepath2) {
+      console.log('Error: missed some file');
+      process.exit(1);
+    }
+    try {
+      const file1 = fileParser(filepath1);
+      const file2 = fileParser(filepath2);
+      const diffTree = compareObjects(file1, file2);
+      console.log(formatCase(diffTree, options.format));
+    } catch (e) {
+      console.error(`Error: ${e.message}`);
+      process.exit(1);
+    }
   })
   .helpOption('-V, --version', 'output the version number')
   .helpOption('-h, --help', 'display help for command');
