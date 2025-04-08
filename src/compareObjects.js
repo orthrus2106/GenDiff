1591fb90c42c9919a22b7df875f443900380d5d1
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-04-08 17:13:48 +0300

    fix: fixed linter errors

--- src/compareObjects.js
+++ src/compareObjects.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+import _, { sortBy } from 'lodash';
 
 const createTree = (key, type, value1 = null, value2 = null, children = null) => ({
   key,
@@ -11,23 +11,21 @@ const createTree = (key, type, value1 = null, value2 = null, children = null) =>
 const compareObjects = (obj1, obj2, depth = 1) => {
   const firstKeys = Object.keys(obj1);
   const secondKeys = Object.keys(obj2);
-  const allKeys = firstKeys.concat(secondKeys);
-  const uniqueKeys = [...new Set(allKeys)].sort();
+  const allKeys = _.union(firstKeys, secondKeys);
+  const uniqueKeys = sortBy(allKeys);
   const result = uniqueKeys.reduce((acc, key) => {
     if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
       const childrens = compareObjects(obj1[key], obj2[key], depth + 1);
-      acc.push(createTree(key, 'nested', null, null, childrens));
-      return acc;
+      return [...acc, createTree(key, 'nested', null, null, childrens)];
     } if (firstKeys.includes(key) && secondKeys.includes(key)) {
       if (obj1[key] === obj2[key]) {
-        acc.push(createTree(key, 'unchanged', obj1[key]));
-      } else {
-        acc.push(createTree(key, 'updated', obj1[key], obj2[key]));
+        return [...acc, createTree(key, 'unchanged', obj1[key])];
       }
-    } else if (firstKeys.includes(key) && !secondKeys.includes(key)) {
-      acc.push(createTree(key, 'removed', obj1[key]));
-    } else if (!firstKeys.includes(key) && secondKeys.includes(key)) {
-      acc.push(createTree(key, 'added', null, obj2[key]));
+      return [...acc, createTree(key, 'updated', obj1[key], obj2[key])];
+    } if (firstKeys.includes(key) && !secondKeys.includes(key)) {
+      return [...acc, createTree(key, 'removed', obj1[key])];
+    } if (!firstKeys.includes(key) && secondKeys.includes(key)) {
+      return [...acc, createTree(key, 'added', null, obj2[key])];
     }
     return acc;
   }, []);
