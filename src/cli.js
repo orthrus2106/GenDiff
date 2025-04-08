1bd05e27ad166577be1ed71c4621b95f7def4e52
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-04-08 16:12:33 +0300

    fix: fixed cli.js

--- src/cli.js
+++ src/cli.js
@@ -10,22 +10,21 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .option('-f, --format [type]', 'output format', 'stylish')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
-  .action((filepath1, filepath2, options) => {
-    if (!filepath1 || !filepath2) {
-      console.log('Error: missed some file');
-      process.exit(1);
+  .action(() => {
+    const { args } = program;
+
+    if (args.length < 2) {
+      console.log('Error: need two files');
+      process.exitCode = 1;
+      return;
     }
-    try {
-      const file1 = fileParser(filepath1);
-      const file2 = fileParser(filepath2);
-      const diffTree = compareObjects(file1, file2);
-      console.log(formatCase(diffTree, options.format));
-    } catch (e) {
-      console.error(`Error: ${e.message}`);
-      process.exit(1);
-    }
+
+    const [filepath1, filepath2] = args;
+    const file1 = fileParser(filepath1);
+    const file2 = fileParser(filepath2);
+    const diffTree = compareObjects(file1, file2);
+    const { format } = program.opts();
+    console.log(formatCase(diffTree, format));
   })
   .helpOption('-V, --version', 'output the version number')
   .helpOption('-h, --help', 'display help for command');
