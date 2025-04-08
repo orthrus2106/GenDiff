00da6acea869493a8bef9754ad28895c51e74bae
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-04-08 16:43:11 +0300

    fix: fixed entry point

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,20 @@
 #!/usr/bin/env node
 
-import '../src/cli.js';
+import { Command } from 'commander';
+import gendiff from '../src/gendiff.js';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2, options) => {
+    console.log(gendiff(filepath1, filepath2, options.format));
+  })
+  .helpOption('-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'display help for command');
+
+program.parse(process.argv);
